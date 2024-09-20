767823e3da34cc3407327af4e8cb4c4b17d25f64
Tuan <tuan@michelangelo.local>
2024-09-20 15:58:35 +1200

    demo app with buggy behaviour

---
+++ demo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace demo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ demo/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace demo;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+
+		var file = await FilePicker.Default.PickAsync();
+	}
+}
+
